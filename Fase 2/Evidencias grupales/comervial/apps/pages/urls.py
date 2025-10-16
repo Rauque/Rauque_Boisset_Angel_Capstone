41ef422549a57a8a3ce5633d76239d80bb944e22
Rauque <128740039+Rauque@users.noreply.github.com>
2025-10-16 16:11:44 -0300

    paginas nuevas

--- Fase 2/Evidencias grupales/comervial/apps/pages/urls.py
+++ Fase 2/Evidencias grupales/comervial/apps/pages/urls.py
@@ -1,7 +1,21 @@
+# apps/pages/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("barandas/", views.barandas, name="barandas"),
+
+    # nuevas
+    path("muebles/", views.muebles, name="muebles"),
+    path("shower-door/", views.shower_door, name="shower_door"),
+    path("mamparas/", views.mamparas, name="mamparas"),
+    path("reposicion-cristales/", views.reposicion_cristales, name="reposicion_cristales"),
+    path("ventanas/corredera/", views.corredera, name="corredera"),
+    path("ventanas/abatible/", views.abatible, name="abatible"),
+    path("ventanas/oscilobatiente/", views.oscilobatiente, name="oscilobatiente"),
+    path("ventanas/proyectante/", views.proyectante, name="proyectante"),
+    path("ventanas/fija/", views.fija, name="fija"),
+    path("ventanas/bow-window/", views.bow_window, name="bow_window"),
+    path("ventanas/medio-punto/", views.medio_punto, name="medio_punto"),
 ]
--- Fase 2/Evidencias grupales/comervial/apps/pages/views.py
+++ Fase 2/Evidencias grupales/comervial/apps/pages/views.py
@@ -5,3 +5,23 @@ def index(request):
 
 def barandas(request):
     return render(request, "pages/barandas.html")
+
+def muebles(request):
+    return render(request, "pages/muebles.html")
+
+def shower_door(request):
+    return render(request, "pages/shower_door.html")
+
+def mamparas(request):
+    return render(request, "pages/mamparas.html")
+
+def reposicion_cristales(request):
+    return render(request, "pages/reposicion_cristales.html")
+
+def corredera(request):       return render(request, "pages/ventanas/corredera.html")
+def abatible(request):        return render(request, "pages/ventanas/abatible.html")
+def oscilobatiente(request):  return render(request, "pages/ventanas/oscilobatiente.html")
+def proyectante(request):     return render(request, "pages/ventanas/proyectante.html")
+def fija(request):            return render(request, "pages/ventanas/fija.html")
+def bow_window(request):      return render(request, "pages/ventanas/bow_window.html")
+def medio_punto(request):     return render(request, "pages/ventanas/medio_punto.html")
